ca2630ae9f32114e15747987cb5ef6d20a796d4e
ABDELGHAFOUR123-h <abdelghafourakaddar10@gmail.com>
2025-05-04 14:55:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-let nbr1 = parseFloat(prompt("entrer le premier nombre"));
-let nbr2 = parseFloat(prompt("entrer la deuxieme nombre"));
 
-let somme = nbr1 + nbr2;
-let difference = nbr1 - nbr2;
-let produit = nbr1 * nbr2;
-let quotient = nbr1 % nbr2;
+const QUESTIONS = [
+    ["Quelle est la capitale de la France ?", "Paris"],
+    ["Combien de pattes a un chat ?", "4"],
+    ["Quelle langue parle-t-on au Brésil ?", "Portugais"],
+    ["Combien font 5 + 3 ?", "8"],
+    ["Quel est le plus grand océan du monde ?", "Pacifique"]
+  ];
+  
+  function lancerQuiz() {
+    let score = 0;
+  
+    for (let i = 0; i < QUESTIONS.length; i++) {
+      let question = QUESTIONS[i][0];
+      let bonneReponse = QUESTIONS[i][1].toLowerCase();
+      let reponseUtilisateur = prompt(question).trim().toLowerCase();
+  
+      if (reponseUtilisateur === bonneReponse) {
+        alert("Réponse juste !");
+        score++;
+      } else {
+        alert("Réponse fausse !");
+      }
+    }
+  
+    alert("Vous avez répondu correctement à " + score + " question(s) sur " + QUESTIONS.length + ".");
+  }
+  
 
-console.log("La somme est : " +somme);
-console.log("Le difference est : "+difference);
-console.log("Le produit est : " +produit);
-console.log("Le quotient est : " +quotient);
